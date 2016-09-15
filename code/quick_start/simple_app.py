a353776a841f6ff94d2ebb727505da57dc05de68
chenguolin <cgl1079743846@gmail.com>
2016-09-15 16:56:47 +0800

    modify file name

---
+++ code/quick_start/simple_app.py
@@ -0,0 +1,14 @@
+"""
+Simple spark app
+"""
+
+from pyspark import SparkContext
+sc = SparkContext("local", "SimpleApp")
+
+#Spark default read from HDFS
+#must be sure has exist HDFS file /user/hadoop/test_data/README.md
+data_rdd = sc.textFile('/user/hadoop/test_data/README.md').cache()
+num_a = data_rdd.filter(lambda line: 'a' in line).count()
+num_b = data_rdd.filter(lambda line: 'b' in line).count()
+print num_a
+print num_b
