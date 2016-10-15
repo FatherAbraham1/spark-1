230f87443aed41d4598c3a35276c6de69da8d774
cgl1079743846@gmail.com <cgl1079743846@gmail.com>
2016-10-15 21:51:47 +0800

    add spark_util

--- code/examples/Spark_0_code.py
+++ code/examples/Spark_0_code.py
@@ -4,15 +4,21 @@
 import os
 import sys
 from pyspark import SparkContext
-sc = SparkContext("Spark_0_code")
+sc = SparkContext(appName="Spark_0_code")
 
-def example_0():
-    text_file = sc.textFile("hdfs://...")
-    counts = text_file.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
-    counts.saveAsTextFile("hdfs://...")
+def example_one():
+    """
+    1. first put data_0.dat to hdfs path /user/hadoop/examples/data_0.dat
+    """
+    raw_data_file = '/user/hadoop/examples/data_0.dat'
+    text_file = sc.textFile(raw_data_file)
+    print text_file.collect()
+    new_text_file = text_file.flatMap(lambda line: line.split(" ")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)
+    result_data_path = '/user/hadoop/examples/data_0_res'
+    new_text_file.saveAsTextFile(result_data_path)
 
 def main():
-    pass
+    example_one()
 
 if __name__ == "__main__":
     try:
